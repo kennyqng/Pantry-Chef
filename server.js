b88a41afffaa8daf93d7e79e70043fda275b2a4c
Evan Dobalian <evandobalian@gmail.com>
2020-06-16 22:25:49 -0700

    making travis happy (camelcase recipe.js / space server.js)

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ require("./routes/api-routes.js")(app);
 require("./routes/dishes-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({force:false}).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
